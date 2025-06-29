17f947f79e5b5f258343884513b90ae767eae721
Dylan de Bruijn <dylandebruyn1993@gmail.com>
2025-06-29 17:50:40 +0100

    Set ndkVersion for Google Maps API

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.example.boat_charge_planner"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
