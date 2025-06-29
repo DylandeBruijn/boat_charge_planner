4c46f351854ab35480dd9860c579d35b4bd4b9f8
Dylan de Bruijn <dylandebruyn1993@gmail.com>
2025-06-29 19:40:12 +0100

    Add pin with infowindow onLongPress

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,28 @@ class BoatChargePlannerApp extends StatefulWidget {
 
 class _BoatChargePlannerAppState extends State<BoatChargePlannerApp> {
   late GoogleMapController mapController;
+  final Set<Marker> _markers = {};
 
   final LatLng _center = const LatLng(54.0, -2.0);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  void _onMapLongPressed(LatLng position) {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('marker_${_markers.length}'),
+          position: position,
+          infoWindow: InfoWindow(
+            title: 'Charge Point ${_markers.length + 1}',
+            snippet:
+                'Lat: ${position.latitude.toStringAsFixed(4)}, Lng: ${position.longitude.toStringAsFixed(4)}',
+          ),
+        ),
+      );
+    });
   }
 
   @override
@@ -31,6 +48,8 @@ class _BoatChargePlannerAppState extends State<BoatChargePlannerApp> {
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
+          onLongPress: _onMapLongPressed,
+          markers: _markers,
           initialCameraPosition: CameraPosition(target: _center, zoom: 6.0),
         ),
       ),
