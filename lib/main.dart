bfe8c4abe8f219403bcfdb3a3ea4fc2476d9069b
Dylan de Bruijn <dylandebruyn1993@gmail.com>
2025-07-01 21:19:13 +0100

    Refactor and rename app and home

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'presentation/screens/map_screen.dart';
 
-Future main() async {
-  runApp(const BoatChargePlannerApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class BoatChargePlannerApp extends StatefulWidget {
-  const BoatChargePlannerApp({super.key});
-
-  @override
-  State<BoatChargePlannerApp> createState() => _BoatChargePlannerAppState();
-}
-
-class _BoatChargePlannerAppState extends State<BoatChargePlannerApp> {
-  late GoogleMapController mapController;
-  final Set<Marker> _markers = {};
-  final LatLng _center = const LatLng(54.0, -2.0);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  void _onMapLongPressed(LatLng position) {
-    setState(() {
-      _markers.add(
-        Marker(
-          markerId: MarkerId('marker_${_markers.length}'),
-          position: position,
-        ),
-      );
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              Icon(Icons.directions_boat, color: Colors.white, size: 28),
-              const SizedBox(width: 8),
-              const Text(
-                'Boat Charge Planner',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color(0xFF1e3c72),
-                  Color(0xFF2a5298),
-                  Color(0xFF4a90e2),
-                ],
-              ),
-            ),
-          ),
-          elevation: 8,
-          shadowColor: Colors.black26,
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          onLongPress: _onMapLongPressed,
-          markers: _markers,
-          initialCameraPosition: CameraPosition(target: _center, zoom: 6.0),
-        ),
+      title: 'Boat Charge Planner',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: const MapScreen(),
     );
   }
 }
